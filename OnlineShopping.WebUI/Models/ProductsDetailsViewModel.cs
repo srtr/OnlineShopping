a54dd21e2fb3950b6189cffbcc486cc26bce84ce
srtr <srtr1992@gmail.com>
2013-12-09 00:57:45 +0800

    product display edited

---
+++ OnlineShopping.WebUI/Models/ProductsDetailsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShopping.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopping.WebUI.Models
+{
+    public class ProductsDetailsViewModel
+    {
+            public Product product { get; set; }
+           // public Manufacturer manufacturer { get; set; }
+           // public Category category { get; set; }
+
+    }
+}
