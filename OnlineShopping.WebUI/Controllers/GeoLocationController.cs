f5f2be007ffaad3cadd30729a2c56c3a69632369
srtr <srtr1992@gmail.com>
2013-12-09 00:58:26 +0800

    store locator v1.0

---
+++ OnlineShopping.WebUI/Controllers/GeoLocationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.WebUI.Controllers
+{
+    public class GeoLocationController : Controller
+    {
+        //
+        // GET: /GeoLocation/
+
+        public string mobileLogin(string userName=null,string password=null)
+        {
+            //return "success";
+            return "{\"firstname\" : saran , \"lastname\" : kumar}";
+        }
+        public ViewResult storeLocator()
+        {
+            return View();
+        }
+    }
+}
+
+
